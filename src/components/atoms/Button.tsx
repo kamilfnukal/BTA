e91633608dddb0756395e06fd30e9bfd8bca749f
DaliborPan <pantlik.dalibor@seznam.cz>
2022-11-17 18:50:50 +0100

    Example lading page

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,11 +1,14 @@
+import { CSSProperties } from 'react'
+
 type ButtonProps = {
   label: string
+  extraClasses?: string
   onClick: () => any
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, label }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, label, extraClasses }) => {
   return (
-    <button className="btn" onClick={onClick}>
+    <button className={`btn ${extraClasses}`} onClick={onClick}>
       {label}
     </button>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,42 @@ const LandingPage: NextPage = () => {
   }, [data])
 
   return (
-    <div className="text-red-700">
-      <Button label="Label" onClick={() => console.log('A')} />
-      <Button onClick={() => signIn('google')} label="Sign in" />
+    <div className="">
+      <div className="hero min-h-screen bg-lighterblue/30">
+        <div className="hero-content flex-col lg:flex-row-reverse w-full lg:gap-x-20">
+          <div className="flex flex-col space-y-10">
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Thursday, 17th November</h2>
+                <p>Today it is completely safe to ride a bike. No accident will happen! 🔥</p>
+                <p>22 °C is comfortable for bike riding. Have fun!</p>
+                <div className="card-actions justify-end mt-4 mb-2">
+                  <Button
+                    extraClasses="bg-blue-800 border-none hover:bg-lighterblue hover:text-blue-800"
+                    onClick={() => signIn('google')}
+                    label="Explore bike accidents and more!"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="max-w-xl">
+            <h1 className="text-6xl font-bold">Brno Bike Transport advisor</h1>
+            <p className="py-6 text-xl text-blue-800/60">
+              We will tell you, when it's safe to ride a bike in Brno. We know the future!
+            </p>
+            <Button
+              extraClasses="bg-blue-800 border-none hover:bg-lighterblue hover:text-blue-800"
+              onClick={() => signIn('google')}
+              label="Sign in"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
