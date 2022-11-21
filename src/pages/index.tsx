37136912319585ce013b0831292eee02bac6eaf3
DaliborPan <pantlik.dalibor@seznam.cz>
2022-11-21 19:41:05 +0100

    Hero adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,10 +14,11 @@ const LandingPage: NextPage = () => {
 
   return (
     <div className="">
-      <div className="hero min-h-screen bg-lighterblue/30">
+      <div className="hero min-h-screen bg-gradient-to-tl from-lighterblue/10 to-lighterblue">
         <div className="hero-content flex-col lg:flex-row-reverse w-full lg:gap-x-20">
           <div className="flex flex-col space-y-10">
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            {/* TODO: Extract to component HeroCard /molecules */}
+            <div className="card card-compact max-w-[384px] bg-base-100 shadow-xl">
               <figure>
                 <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
               </figure>
@@ -29,7 +30,7 @@ const LandingPage: NextPage = () => {
                   <Button
                     extraClasses="bg-blue-800 border-none hover:bg-lighterblue hover:text-blue-800"
                     onClick={() => signIn('google')}
-                    label="Explore bike accidents and more!"
+                    label="Sign in!"
                   />
                 </div>
               </div>
@@ -37,15 +38,42 @@ const LandingPage: NextPage = () => {
           </div>
 
           <div className="max-w-xl">
-            <h1 className="text-6xl font-bold">Brno Bike Transport advisor</h1>
-            <p className="py-6 text-xl text-blue-800/60">
-              We will tell you, when it's safe to ride a bike in Brno. We know the future!
+            <h1 className="text-6xl font-bold">Brno Bike Advisor</h1>
+            <p className="bg-lighterblue/10 shadow-md rounded-lg py-2 px-4 my-4 text-blue-800/80">
+              We will tell you, when it's safe to ride a bike in Brno. Sign in and see, when and where will be the next
+              bike accident. We know the future!
             </p>
-            <Button
-              extraClasses="bg-blue-800 border-none hover:bg-lighterblue hover:text-blue-800"
-              onClick={() => signIn('google')}
-              label="Sign in"
-            />
+
+            <h2 className="text-3xl pb-4 pt-4 font-semibold text-blue-800 mt-10">Accidents</h2>
+            {/* TODO: Extract to component HeroStats /molecules */}
+            <div className="stats stats-vertical lg:stats-horizontal shadow">
+              {/* TODO: Extract to component Stat /atoms */}
+              <div className="stat">
+                <div className="stat-title">This month</div>
+                <div className="stat-value">15</div>
+                <div className="stat-desc">↗︎ 1</div>
+              </div>
+
+              <div className="stat">
+                <div className="stat-title">This year</div>
+                <div className="stat-value">210</div>
+                <div className="stat-desc">↗︎ 12 (22%)</div>
+              </div>
+
+              <div className="stat">
+                <div className="stat-title">All time</div>
+                <div className="stat-value">1,312</div>
+                <div className="stat-desc">↘︎ 90 (14%)</div>
+              </div>
+            </div>
+
+            <div className="pt-8">
+              <Button
+                extraClasses="bg-blue-800 border-none hover:bg-lighterblue hover:text-blue-800"
+                onClick={() => signIn('google')}
+                label="Explore bike accidents and more!"
+              />
+            </div>
           </div>
         </div>
       </div>
