ef34d1e8c7f710401c2a0a77a26a2f68066d70c2
kamil <xfnukal>
2022-11-28 17:10:59 +0100

    HeroStats

--- src/components/atoms/HeroStats.tsx
+++ src/components/atoms/HeroStats.tsx
@@ -9,9 +9,9 @@ type HeroStatsProps = {
 const HeroStats: React.FC<HeroStatsProps> = ({ thisMonth, thisYear, allTime }) => {
   return (
     <div className="stats stats-vertical lg:stats-horizontal shadow">
-      <Stat title={thisMonth.title} value={thisMonth.value} desc={thisMonth.value} />
-      <Stat title={thisYear.title} value={thisYear.value} desc={thisYear.value} />
-      <Stat title={allTime.title} value={allTime.value} desc={allTime.value} />
+      <Stat {...thisMonth} />
+      <Stat {...thisYear} />
+      <Stat {...allTime} />
     </div>
   )
 }
