0222a1565a38dce5bd66ece72a0d540629c83c98
kamil <xfnukal>
2022-12-05 18:59:39 +0100

    todayDate

--- src/modules/HomeModule.tsx
+++ src/modules/HomeModule.tsx
@@ -1,15 +1,20 @@
+import { useCallback } from 'react'
 import { Calendar } from 'react-feather'
 import { HomeDayCard } from '../components/molecules'
 import { HomePageProps } from '../types'
 
 const HomeModule: React.FC<HomePageProps> = ({ yesterday, today, tomorrow }) => {
+  const getCurrentDate = useCallback(() => {
+    const t = new Date('2020-12-28').toLocaleString('en-us', { month: 'long', day: 'numeric' })
+    return t
+  }, [])
+
   return (
     <div>
       <div className="container lg:px-10 flex flex-col w-full mx-auto relative">
         <div className="absolute bg-gradient-to-br from-lighterpink/80 to-lightpink rounded-lg py-2 px-4 -top-4 flex space-x-4 font-medium">
           <Calendar />
-          {/* TODO: Today's date */}
-          <span>17th November</span>
+          <span>{ getCurrentDate() }</span>
         </div>
 
         <div className="flex justify-center w-full">
