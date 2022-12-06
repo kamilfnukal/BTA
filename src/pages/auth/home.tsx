c3120d41560d250dd46cc9c3d62836886609df35
DaliborPan <pantlik.dalibor@seznam.cz>
2022-12-06 08:16:34 +0100

    feat: Extracting getHomeData

--- src/pages/auth/home.tsx
+++ src/pages/auth/home.tsx
@@ -1,30 +1,8 @@
 import { GetStaticProps, NextPage } from 'next'
 import { useEffect } from 'react'
-import { YEAR_OFFSET } from '../../const'
-import { getBrnoBikeAccidents } from '../../hooks/accidents'
-import { getPrecipitation, getTemperature } from '../../hooks/weather'
+import { getHomeData } from '../../hooks/weather'
 import { HomeModule, ProtectedModule } from '../../modules'
 import { HomePageProps } from '../../types'
-import { BrnoBikeAccidentsResponse, WeatherTemperatureResponse } from '../../types/api'
-
-const filterWeatherByDate = (weatherResponse: WeatherTemperatureResponse, date: Date) => {
-  return weatherResponse[date.getMonth()][date.getDate()]
-}
-
-const filerAccidentsByDate = (accidentsResponse: BrnoBikeAccidentsResponse, date: Date) => {
-  // TODO: fix via `datum`
-  return accidentsResponse.filter(
-    ({ attributes: { den, mesic, rok } }) =>
-      den === date.getDate() && mesic === date.getMonth() && rok === date.getFullYear() - YEAR_OFFSET
-  )
-}
-
-const isSameYear = (d1: Date, d2: Date) => d1.getFullYear() === d2.getFullYear()
-
-const fetchWeatherDifferentYear = async (date: Date) => {
-  const year = date.getFullYear() - YEAR_OFFSET
-  return [await getTemperature(year), await getPrecipitation(year)]
-}
 
 // TODO: useEffect that checks if needing revalidate (current rendered day stored in firestore)
 const HomePage: NextPage<HomePageProps> = (props) => {
@@ -45,48 +23,8 @@ const HomePage: NextPage<HomePageProps> = (props) => {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const now = new Date()
-  const yesterday = new Date()
-  const tomorrow = new Date()
-
-  yesterday.setDate(now.getDate() - 1)
-  tomorrow.setDate(now.getDate() + 1)
-
-  const [temperatureToday, precipicationToday, accidents] = await Promise.all([
-    getTemperature(now.getFullYear() - YEAR_OFFSET),
-    getPrecipitation(now.getFullYear() - YEAR_OFFSET),
-    getBrnoBikeAccidents()
-  ])
-
-  let yesterdayWeather = [temperatureToday, precipicationToday]
-  let tomorrowWeather = [temperatureToday, precipicationToday]
-
-  if (!isSameYear(now, yesterday)) {
-    yesterdayWeather = await fetchWeatherDifferentYear(yesterday)
-  }
-
-  if (!isSameYear(now, tomorrow)) {
-    tomorrowWeather = await fetchWeatherDifferentYear(tomorrow)
-  }
-
   return {
-    props: {
-      yesterday: {
-        temperature: filterWeatherByDate(yesterdayWeather[0], yesterday),
-        precipitation: filterWeatherByDate(yesterdayWeather[1], yesterday),
-        accidents: filerAccidentsByDate(accidents, yesterday)
-      },
-      today: {
-        temperature: filterWeatherByDate(temperatureToday, now),
-        precipitation: filterWeatherByDate(precipicationToday, now),
-        accidents: filerAccidentsByDate(accidents, now)
-      },
-      tomorrow: {
-        temperature: filterWeatherByDate(tomorrowWeather[0], tomorrow),
-        precipitation: filterWeatherByDate(tomorrowWeather[1], tomorrow),
-        accidents: filerAccidentsByDate(accidents, tomorrow)
-      }
-    }
+    props: await getHomeData()
   }
 }
 
