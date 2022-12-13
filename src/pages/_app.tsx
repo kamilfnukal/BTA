6686e903b1b095e3f2a910d139e2c8e6b8fce379
Patrícia Andicsová <patricia.andicsova@bluelemons.sk>
2022-12-13 20:07:14 +0100

    _app.tsx saved

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,21 @@ const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps<{ ses
           href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <title>Brno Bike Advisor</title>
+        <meta name="title" content="Brno Bike Advisor"/>
+        <meta name="description" content="We will tell you, when it's safe to ride a bike in Brno. Sign in and see, when and where will be the next bike accident. We know the future!"/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://brnotransport.vercel.app/"/>
+        <meta property="og:title" content="Brno Bike Advisor"/>
+        <meta property="og:description" content="We will tell you, when it's safe to ride a bike in Brno. Sign in and see, when and where will be the next bike accident. We know the future!"/>
+        <meta property="og:image" content="https://brnotransport.vercel.app/seo.png"/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://brnotransport.vercel.app/"/>
+        <meta property="twitter:title" content="Brno Bike Advisor"/>
+        <meta property="twitter:description" content="We will tell you, when it's safe to ride a bike in Brno. Sign in and see, when and where will be the next bike accident. We know the future!"/>
+        <meta property="twitter:image" content="https://brnotransport.vercel.app/seo.png"/>
       </Head>
       <SessionProvider session={session}>
         <Component {...pageProps} />
